7eb38932cc8ce942d63e40e51162bc2664f877b5
Kellen Kolbeck & Amit Mhatre <student@epicodus.com>
2015-10-12 17:05:24 -0700

    task controller actions syntax corrected

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,38 +4,35 @@ class TasksController < ApplicationController
     @task=@list.tasks.new
     # render :new
   end
+
   def create
     @list = List.find(params[:list_id])
     @task = @list.tasks.new(task_params)
     if @task.save
-
       redirect_to list_path(@task.list)
     else
       render :new
+    end
   end
+
   def edit
     @list = List.find(params[:list_id])
     @task= Task.find(params[:id])
-
     render :edit
   end
+
   def update
     @list= List.find(params[:list_id])
-
     @task= Task.find(params[:id])
-    binding.pry
-    if @task.update(description: params[:description])
-      binding.pry
-      redirect_to list_path(@list)
+    if @task.update(task_params)
+      redirect_to list_path(@task.list)
     else
       render :edit
     end
-
   end
 
-end
+  private
 
-private
   def task_params
     params.require(:task).permit(:description)
   end
